9a1536d566a39651fac4055aded8b622dd9f51ae
Hector Sanchez <hsanchez7934@gmail.com>
2018-02-20 15:31:28 -0700

    Add static assets file

---
+++ src/staticAssets.js
@@ -0,0 +1,92 @@
+import cssLogo from './assets/csslogo.svg';
+import es6Logo from './assets/es6logo.svg';
+import htmlLogo from './assets/htmllogo.svg';
+import jqueryLogo from './assets/jquerylogo.svg';
+import jsLogo from './assets/jslogo.svg';
+import knexLogo from './assets/knexlogo.svg';
+import nodeJSLogo from './assets/nodejslogo.svg';
+import reactLogo from './assets/reactlogo.svg';
+import reduxLogo from './assets/reduxlogo.svg';
+import routerLogo from './assets/reduxlogo.svg';
+import sassLogo from './assets/sasslogo.svg';
+import sqlLogo from './assets/sqllogo.svg';
+import expressLogo from './assets/expresslogo.svg';
+
+import chaiLogo from './assets/chailogo.svg';
+import jestLogo from './assets/jestlogo.svg';
+import enzymeLogo from './assets/enzymelogo.svg';
+import mochaLogo from './assets/mochalogo.svg';
+
+export const techStackArray = [
+  {
+    logo: cssLogo,
+     name: 'CSS3'
+  },
+  {
+    logo: es6,
+    name: 'ES6'
+  },
+  {
+    logo: htmlLogo,
+    name: 'HTML5'
+  },
+  {
+    logo: jqueryLogo,
+    name: 'jQuery'
+  },
+  {
+    logo: jsLogo,
+    name: 'JavaScript'
+  },
+  {
+    logo: knexLogo,
+    name: 'Knex.js'
+  },
+  {
+    logo: nodeJSLogo,
+    name: 'Node.js'
+  },
+  {
+    logo: reactLogo,
+    name: 'React.js'
+  },
+  {
+    logo: reduxlogo,
+    name: 'Redux'
+  },
+  {
+    logo: routerLogo,
+    name: 'React Router'
+  },
+  {
+    logo: sassLogo,
+    name: 'SASS'
+  },
+  {
+    logo: sqlLogo,
+    name: 'PostgreSQL'
+  },
+  {
+    logo: expressLogo,
+    name: 'Express.js'
+  }
+];
+
+export const testStackArray = [
+  {
+    logo: chaiLogo,
+    name: 'Chai'
+  },
+  {
+    logo: mochaLogo,
+    name: 'Mocha'
+  },
+  {
+    logo: jestLogo,
+    name: 'Jest'
+  },
+  {
+    logo: enzymeLogo,
+    name: 'Enzyme'
+  }
+];
