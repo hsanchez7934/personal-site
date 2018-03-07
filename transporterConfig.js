562b0d316e4f7a701a6a6ad17cf17b4d368169d1
Hector Sanchez <hsanchez7934@gmail.com>
2018-03-07 01:58:33 -0700

    Remove keys, back to variables

--- transporterConfig.js
+++ transporterConfig.js
@@ -10,6 +10,6 @@ module.exports = {
   user,
   clientId: clientID.web.client_id,
   clientSecret: clientID.web.client_secret,
-  refreshToken: `1/ISFHS_cylBFn-2NSEEVCAZaJpZwKFRbp_nQzWGDJMgI`,
-  accessToken: `ya29.Glt1Bc2_LluWM3jsN7D4Bw5Eu7vQwxrw-pqw5cMeyW628qmIr14Y9ZiBWH5Boib2w7Yvf0EiA8Dl8en0JTwFIrF8WTtqL8rS-mUEMeTZHQgKxbJRxGw7cuX0mUFk`
+  refreshToken,
+  accessToken,
 }
