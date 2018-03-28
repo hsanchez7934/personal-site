5b67fa82954d54efc1d21a3f7b324802310302e5
Hector Sanchez <hsanchez7934@gmail.com>
2018-03-28 02:03:12 -0600

    Remove useless scripts from package json

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './components/App/App.css';
 import App from './components/App/App.js';
 import registerServiceWorker from './registerServiceWorker';
-require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
