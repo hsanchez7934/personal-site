97b3e280cb69f84f6631370df1d3f07c96fc5e12
Hector Sanchez <hsanchez7934@gmail.com>
2018-03-28 00:08:37 -0600

    Add server.js and transporterConfig file back

--- transporterConfig.js
+++ transporterConfig.js
@@ -1,11 +1,11 @@
-// require('dotenv').config();
-//
-// const type = process.env.SERVICE;
-// const user = process.env.GMAIL_USER;
-// const pass = process.env.GMAIL_PASS;
-//
-// module.exports = {
-//   type,
-//   user,
-//   pass
-// }
+require('dotenv').config();
+
+const type = process.env.SERVICE;
+const user = process.env.GMAIL_USER;
+const pass = process.env.GMAIL_PASS;
+
+module.exports = {
+  type,
+  user,
+  pass
+}
